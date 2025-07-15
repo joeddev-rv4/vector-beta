90b27071a4d5f3e2269c17f180767ca664db9379
joeddev-rv4 <jrojas@rvcuatro.com>
2025-07-15 12:30:30 -0600

    adding update notification

--- api/v1/endpoints/users.py
+++ api/v1/endpoints/users.py
@@ -5,4 +5,4 @@ router = APIRouter()
 @router.get("/{user_id}")
 def read_user(user_id: str):
     print("prueba de read_user" + user_id)
-    return(user_id)
+    return(user_id + " actualización 0.1.1")
